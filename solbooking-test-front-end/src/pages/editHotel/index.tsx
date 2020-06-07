0d21aa82109e9b64c2fdf504831afc93fc8d6ecc
Felipe Ruiz <felipe.r.p.1994@gmail.com>
2020-06-07 20:56:55 +0200

    Edit hotel component

---
+++ solbooking-test-front-end/src/pages/editHotel/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { Hotel, getDefaultHotel } from '../../model/hotel';
+import { hotelService } from '../../services/hotelService';
+import { Paper, TextField, Button } from '@material-ui/core';
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
+
+import './styles.scss';
+import { validateEmail } from '../../utils/validations';
+import { routes } from '../../utils/routes';
+
+export const EditHotel = (props: any) => {
+
+    const [hotel, setHotel] = useState<Hotel>(getDefaultHotel());
+
+    const history = useHistory();
+
+    useEffect(() => {
+        hotelService.getHotelById(+props.match.params.id).then((hotel) => setHotel(hotel));
+    }, [props.match.params.id]);
+
+    const editField = (event: any) => {
+        
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setHotel({
+            ...hotel,
+            [name]: value
+        });
+    }
+
+    const saveHotel = (_: any) => {
+
+        if (hotel.name === '' || hotel.address === '' || hotel.phone === '' || hotel.mail === '') {
+
+            toast('Todos los campos son obligatorios', { type: 'error' });
+        } else if (!validateEmail(hotel.mail)) {
+             
+            toast('Introduzca un email correcto', { type: 'error' });
+        } else {
+            hotelService.updateHotel(hotel).then((result) => {
+
+                toast('Hotel guardado correctamente', { type: 'success' });
+
+                history.push(routes.home);
+            });
+        }
+    }
+
+    return (
+        <Fragment>
+            {
+                hotel &&
+                (
+                    <Paper elevation={2} className="container">
+                        <TextField className="input" name="name" label="Nombre" value={hotel.name} onChange={editField} />
+                        <TextField className="input" name="address" label="Dirección" value={hotel.address} onChange={editField} />
+                        <TextField className="input" name="phone" label="Teléfono" value={hotel.phone} onChange={editField} />
+                        <TextField className="input" name="mail" label="Mail" value={hotel.mail} onChange={editField} />
+                        <Button variant="contained" color="primary" onClick={saveHotel}>
+                            Guardar
+                        </Button>
+                    </Paper>
+                )
+            }
+        </Fragment>
+    );
+
+}
